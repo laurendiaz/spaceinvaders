7acd723148396e827fe2255c479d528a3f72c438
Lauren Diaz <laurendiaz@lauren.local>
2020-09-25 15:04:25 -0400

    adding initial models

---
+++ model/Missile.js
@@ -0,0 +1,9 @@
+import Sprite from "./Sprite.js";
+
+class Missile extends Sprite {
+    constructor(x, y, height, color) {
+        super (x, y, width, height, color);
+    }
+}
+
+export default Missile;
