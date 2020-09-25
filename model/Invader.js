f47e9cfe107bdd9d0b9b47c5fa043aca6d2ba619
Lauren Diaz <laurendiaz@lauren.local>
2020-09-25 15:07:48 -0400

    also forgot invader i am bad at git so sorry

---
+++ model/Invader.js
@@ -0,0 +1,9 @@
+import Sprite from "./Sprite.js";
+
+class Invader extends Sprite {
+    constructor(x, y, height, color) {
+        super (x, y, width, height, color);
+    }
+}
+
+export default Invader;
