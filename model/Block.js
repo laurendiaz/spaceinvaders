f1432208cc6939f28d4e0b72945eaa4b3522c691
Lauren Diaz <laurendiaz@lauren.local>
2020-09-30 14:03:10 -0400

    expanding script

--- model/Block.js
+++ model/Block.js
@@ -10,7 +10,7 @@ class Block {
     draw(ctx) {
       ctx.beginPath();
       ctx.rect(this.x, this.y, this.width, this.height);
-      ctx.fillStyle = this.image;
+      //ctx.fillStyle = this.image;
       ctx.fill();
       ctx.closePath();
     }
--- script.js
+++ script.js
@@ -23,27 +23,55 @@ import Tank from "./model/Tank.js";
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-// const img = new Image(50, 50);
-// img.src = "./assets/tank.png";
-
-tank
+// tank
 const timage = new Image(50, 50);
 timage.src = "./assets/tank.png";
 const tank = new Tank(canvas.width/2, canvas.height - 30, 10, timage);
 
-// invader
-const invader = new Image(50, 50);
-invader.src = "./assets/invader.png";
+// are these going to be constants??
+// // invader
+// const iimage = new Image(50, 50);
+// iimage.src = "./assets/invader.png";
+// const invader = new Invader(canvas.width*Math.random(), canvas.height, 10, iimage);
 
-// missile
-const missile = new Image(50, 50);
-missile.src = "./assets/missile.png"
+// // missile
+// const mimage = new Image(50, 50);
+// mimage.src = "./assets/missile.png";
+// // make 10 missiles? or just one and call it max 10 times?
+
+const invaders = [];
+const maxMissile = 10;
+//is there a max number of invaders that will be on the canvas at once?
+
+let count = 0;
+while(count < 10) {
+
+}
+
+let score = 0;
 
 // draw canvas
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  Tank.draw(ctx);
+  Tank.move(canvas.width);
+  Missile.collides(Invader);
+  invaders.forEach((Invader) => {
+    Invader.draw(ctx);
+    score += Invader.collides(Missile);
+  });
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("Score: " + score, 8, 20);
   ctx.drawImage(img, canvas.width / 2 - 25, canvas.height - 60, 50, 50);
+  // what does this do??
   window.requestAnimationFrame(draw);
 }
+
+const interval = setInterval(draw, 10);
+document.addEventListener("gameover", (_e) => {
+  clearInterval(interval);
+  window.alert("Game Over!");
+});
 
 draw();
